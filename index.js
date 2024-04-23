073f7da49f6cb9f692ccff03ad4940fdf35e168b
ZIIM <johntgause12@gmail.com>
2024-04-22 22:49:04 -0700

    testing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 'use strict';
+const express = require('express');
+const app = express();
+const port = 3004;
+const cors = require('cors');
+
+app.use(cors());
+
+app.get("/", (req, res) =>{
+    res.send("Hello world")
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
+});
